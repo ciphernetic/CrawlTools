ae5203aa44c50e392ddf4603df181fa7def14bdb
Mark Donahue <ciphernetic+github@gmail.com>
2013-06-15 09:22:00 -0500

    Added unique species won list to report.

--- Scraper/CLI/Program.cs
+++ Scraper/CLI/Program.cs
@@ -35,6 +35,8 @@ namespace Crawl.Scraper.CLI
 				player.Wins.Select(w => w.Character).Distinct().Count());
 			sb.AppendFormat("Unique gods won: {0}\n",
 				PlayerUtils.GetWonGods(player.Wins));
+			sb.AppendFormat("Unique species won: {0}\n",
+				PlayerUtils.GetWonSpecies(player.Wins));
 
 			return sb.ToString();
 		}
