d223d19083d03c16365a07f3c8229f989e346026
Mark Donahue <ciphernetic+github@gmail.com>
2013-06-14 22:34:51 -0500

    Added comments to player stats class.

--- Scraper/PlayerStats.cs
+++ Scraper/PlayerStats.cs
@@ -15,6 +15,10 @@ namespace Crawl.Scraper
 
 		HtmlDocument page;
 
+		/// <summary>
+		/// Base constructor.
+		/// </summary>
+		/// <param name="playerName"></param>
 		public PlayerStats(string playerName)
 		{
 			PlayerName = playerName;
@@ -24,6 +28,10 @@ namespace Crawl.Scraper
 			ReadWins(page);
 		}
 
+		/// <summary>
+		/// Determines if the player has one with every available god.
+		/// </summary>
+		/// <returns>True if players has won with every god.</returns>
 		public bool IsPolytheist()
 		{
 			God gods = 0;
@@ -41,6 +49,10 @@ namespace Crawl.Scraper
 			return gods == God.All;
 		}
 
+		/// <summary>
+		/// Loads page.
+		/// </summary>
+		/// <returns>Returns loaded page.</returns>
 		private HtmlDocument LoadPage()
 		{
 			HtmlWeb web = new HtmlWeb();
@@ -48,6 +60,10 @@ namespace Crawl.Scraper
 				BASE_URL, PlayerName));
 		}
 
+		/// <summary>
+		/// Parses the wins tables.
+		/// </summary>
+		/// <param name="doc">Page to parse.</param>
 		private void ReadWins(HtmlDocument doc)
 		{
 			foreach(HtmlNode win in doc.DocumentNode
@@ -61,7 +77,7 @@ namespace Crawl.Scraper
 					NumberStyles.AllowThousands);
 				w.God = win.ChildNodes[7].InnerText;
 				w.NumberOfRunes = Convert.ToInt32
-					(win.ChildNodes[8].InnerText.Trim() == string.Empty
+					(win.ChildNodes[8].InnerText.Trim() == string.Empty // old games runes were inventory items, sometimes people forgot to pick them up...elliptic.
 						? "0"
 						: win.ChildNodes[8].InnerText);
 				w.Version = win.ChildNodes[10].InnerText;
