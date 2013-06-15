ca428f74111d0a3bea08c2f670a1d58a26e60926
Mark Donahue <ciphernetic+github@gmail.com>
2013-06-15 09:27:00 -0500

    Revert "Added function to calc distinctly won species."
    This reverts commit e2fba8d7afb185ebc53aa807bf9f9d4e22fb6881.

--- Scraper/PlayerUtils.cs
+++ Scraper/PlayerUtils.cs
@@ -30,33 +30,5 @@ namespace Crawl.Scraper
 
 			return rtn;
 		}
-
-		/// <summary>
-		/// Returns a flagged enum of all won species. Renamed species return
-		/// as current name.
-		/// </summary>
-		/// <param name="wins">All wins to analyze.</param>
-		/// <returns>Flagged Species enum.</returns>
-		public static Species GetWonSpecies(IEnumerable<Win> wins)
-		{
-			Species rtn = 0;
-
-			foreach(Species s in wins
-				.Select(w => (Species)Enum.Parse(typeof(Species),
-					w.Character.Substring(0, 2)))
-				.Distinct())
-			{
-				if(Species.Ke == s)
-				{
-					rtn |= Species.Te;
-				}
-				else
-				{
-					rtn |= s;
-				}
-			}
-
-			return rtn;
-		}
 	}
 }
