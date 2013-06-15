bb377cc5d23857e9b7536f44d721a5c76c070e39
Mark Donahue <ciphernetic+github@gmail.com>
2013-06-15 14:40:49 -0500

    Added overload for IsGreatPlayer for trunk.
    Added missing comments.

--- Scraper/Achievements.cs
+++ Scraper/Achievements.cs
@@ -24,11 +24,35 @@ namespace Crawl.Scraper
 			return wins.Count >= 10;
 		}
 
+		/// <summary>
+		/// Determines if the player has won every species in a given version.
+		/// </summary>
+		/// <param name="version">Version to check.</param>
+		/// <param name="wins">Wins to analyze.</param>
+		/// <returns>True if they have won all species for version.</returns>
 		public static bool IsGreatPlayer(Version version, List<Win> wins)
 		{
 			return GreatPlayerProgress(version, wins).Count == 0;
 		}
 
+		/// <summary>
+		/// Determines if the player has won every species for Trunk.
+		/// </summary>
+		/// <param name="wins">Wins to analyze.</param>
+		/// <returns>True if they have won all species for Trunk.</returns>
+		public static bool IsGreatPlayer(List<Win> wins)
+		{
+			return IsGreatPlayer(Data.Species.GetSpecies().Values
+				.OrderByDescending(s => s.FirstVersion)
+				.First().FirstVersion, wins);
+		}
+
+		/// <summary>
+		/// List all unwon species for a given version.
+		/// </summary>
+		/// <param name="version">Version to check.</param>
+		/// <param name="wins">Wins to analyze.</param>
+		/// <returns>List of unwon species.</returns>
 		public static List<Species> GreatPlayerProgress(Version version, List<Win> wins)
 		{
 			return (from versionSpecies in Utils.GetAllSpecies(version)
