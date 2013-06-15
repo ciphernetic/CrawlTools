656fb3d8e58ef76bd4e91e97928e9638c03b9817
Mark Donahue <ciphernetic+github@gmail.com>
2013-06-14 22:45:29 -0500

    Created report for CLI.

--- Scraper/CLI/Program.cs
+++ Scraper/CLI/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Text;
 
 using Crawl.Scraper;
 
@@ -8,9 +10,21 @@ namespace Crawl.Scraper.CLI
 	{
 		static void Main(string[] args)
 		{
-			PlayerStats player = new PlayerStats("elliptic");
-			Console.WriteLine("{0} is a polytheist - {1}", player.PlayerName,
-				player.IsPolytheist());
+			PlayerStats player = new PlayerStats("palyth");
+			Console.WriteLine(BuildReport(player));
+		}
+
+		static string BuildReport(PlayerStats player)
+		{
+			StringBuilder sb = new StringBuilder();
+			sb.AppendFormat("Player: {0}\n", player.PlayerName);
+			sb.AppendFormat("Number of wins: {0}\n", player.Wins.Count);
+			sb.AppendFormat("Is good player: {0}\n", player.IsGoodPlayer());
+			sb.AppendFormat("Is polytheist: {0}\n", player.IsPolytheist());
+			sb.AppendFormat("Unique combos won: {0}\n",
+				player.Wins.Select(w => w.Character).Distinct().Count());
+
+			return sb.ToString();
 		}
 	}
 }
