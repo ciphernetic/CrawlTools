9aab53d3e4447dba6c420b7d3bffffb367a0afac
Mark Donahue <ciphernetic+github@gmail.com>
2013-06-15 14:10:01 -0500

    Added function that would return all species for a given version.

---
+++ Scraper/Utils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Crawl.Scraper
+{
+	public static class Utils
+	{
+		public static IEnumerable<Species> GetAllSpecies(Version version)
+		{
+			return Data.Species.GetSpecies().Values
+				.Where(s => (s.Active && s.FirstVersion.CompareTo(version) <= 0)
+					|| (!s.Active && s.LastVersion.CompareTo(version) >= 0));
+		}
+	}
+}
